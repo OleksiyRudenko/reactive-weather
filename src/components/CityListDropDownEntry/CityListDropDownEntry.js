fd6934c5367031d9e500ef3d2114b3dee04173e3
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-05-14 15:37:31 +0300

    search: dropdown: Do not select any option until Down key pressed

--- src/components/CityListDropDownEntry/CityListDropDownEntry.js
+++ src/components/CityListDropDownEntry/CityListDropDownEntry.js
@@ -4,22 +4,24 @@ import './CityListDropDownEntry.css';
 export default class CityListDropDown extends Component {
   render() {
     const props = this.props;
-    // console.log('CLDDE.render', this.props);
+    console.log('CLDDE.render', this.props, !props.id && props.setFocus);
     return (
       <label htmlFor={'city-list-dd-item-'+props.id}>
-        <input type='radio' name='city-list-dd-selection' id={'city-list-dd-item-'+props.id} value={props.text}
-               className='city-list-dd-item-radio' defaultChecked={!props.id}
+        <input type='radio' name='city-list-dd-selection' id={'city-list-dd-item-'+props.id} defaultValue={props.text}
+               className='city-list-dd-item-radio' defaultChecked={props.setFocus} autoFocus={props.setFocus}
                ref={element => this.inputControl = element}
         />
-        <div className='city-list-dd-item'><i className='material-icons'>{props.icon}</i>{props.text}</div>
+        <div className='city-list-dd-item'><i className='material-icons'>{props.icon}</i> {props.text}</div>
       </label>
     );
   }
   componentDidMount() {
+    console.log('CLDDE.didMount', this.props, this.inputControl, this.inputControl.focus);
     this.props.setFocus && this.inputControl.focus();
   }
 
   componentDidUpdate() {
+    console.log('CLDDE.didUpdate', this.props, this.inputControl, this.inputControl.focus);
     this.props.setFocus && this.inputControl.focus();
   }
 }
