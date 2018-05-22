1d194dd03c8aedad9d568e791e39acc16db9d5ac
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-05-22 09:15:12 +0300

    App: Add 'Llanfairpwllgwyngyll,GB' to History service preset list

--- src/services/CityHistoryService/config.js
+++ src/services/CityHistoryService/config.js
@@ -2,7 +2,8 @@ export const config = {
   storeName: 'cityhistory',
   historyInitialSet: [
     { name: 'Kyiv,UA',   nameUC: 'KYIV,UA',   lastQueried: + new Date(), },
-    { name: 'London,UK', nameUC: 'LONDON,UK', lastQueried: + new Date(), },
+    { name: 'London,GB', nameUC: 'LONDON,GB', lastQueried: + new Date(), },
+    { name: 'Llanfairpwllgwyngyll,GB', nameUC: 'LLANFAIRPWLLGWYNGYLL,GB', lastQueried: + new Date(), },
     { name: 'Odessa,UA', nameUC: 'ODESSA,UA', lastQueried: + new Date(), },
     { name: 'Odessa,US', nameUC: 'ODESSA,US', lastQueried: + new Date(), },
   ],
